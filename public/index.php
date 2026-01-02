cac1aef1a088b6ce7179a5fd75f6a86d8af3c190
Muhammad Zacky <81474961+Muhammad-Zacky@users.noreply.github.com>
2026-01-03 06:28:17 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,21 +7,23 @@ define('LARAVEL_START', microtime(true));
 // 1. Register Autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// 2. Siapkan folder Storage & Cache di /tmp (Khusus Vercel)
+// 2. Persiapkan Folder di /tmp (KHUSUS VERCEL)
 if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'production') {
     $tmpDir = '/tmp/laravel';
-    $subDirs = [
-        $tmpDir . '/storage/framework/views',
-        $tmpDir . '/storage/framework/cache',
-        $tmpDir . '/storage/framework/sessions',
+    $dirs = [
         $tmpDir . '/bootstrap/cache',
+        $tmpDir . '/storage/framework/views',
+        $tmpDir . '/storage/framework/sessions',
+        $tmpDir . '/storage/framework/cache',
     ];
 
-    foreach ($subDirs as $dir) {
-        if (!is_dir($dir)) mkdir($dir, 0777, true);
+    foreach ($dirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
     }
 
-    // Paksa Laravel pakai path bootstrap cache di /tmp
+    // PAKSA Laravel menulis file penemuan paket (discovery) ke /tmp
     putenv("APP_PACKAGES_CACHE={$tmpDir}/bootstrap/cache/packages.php");
     putenv("APP_SERVICES_CACHE={$tmpDir}/bootstrap/cache/services.php");
     putenv("APP_CONFIG_CACHE={$tmpDir}/bootstrap/cache/config.php");
@@ -31,7 +33,7 @@ if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'production') {
 // 3. Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// 4. Set Storage Path ke /tmp jika di Vercel
+// 4. Set Path Storage ke /tmp
 if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'production') {
     $app->useStoragePath('/tmp/laravel/storage');
 }
