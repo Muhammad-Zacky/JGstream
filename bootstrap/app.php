9e7cbf68fcabd7bf3b96b44ad7cca5b988aec306
Muhammad Zacky <81474961+Muhammad-Zacky@users.noreply.github.com>
2026-01-03 06:40:25 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\View\ViewServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->registered(function ($app) {
+        // PAKSA Laravel mengenali sistem VIEW di Vercel
+        $app->register(ViewServiceProvider::class);
+    })
+    ->create();
